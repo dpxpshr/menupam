ce63ffb29c7043138be5590ffe8294322751b6b7
lee5031207 <lee5031207@naver.com>
2021-04-15 13:41:52 +0900

    [이성욱] websocket테스트

---
+++ menupam/src/main/java/com/kh/toy/common/socket/handler/EchoHandler.java
@@ -0,0 +1,61 @@
+package com.kh.toy.common.socket.handler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.kh.toy.member.model.vo.Member;
+
+public class EchoHandler extends TextWebSocketHandler{
+
+	//접속한 세션들을 List에 넣어줄 리스트
+	List<WebSocketSession> sessions = new ArrayList<WebSocketSession>();
+	
+	//세션 아이디와 회원 아이디를 저장할 리스트
+	Map<String , WebSocketSession> userSessionMap = new HashMap<String, WebSocketSession>();
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		// 연결 시작할때 동작하는 메서드
+		sessions.add(session);
+		userSessionMap.put(getUserId(session), session);
+		System.out.println("연결 성공 : "+userSessionMap.toString());
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		// 연결 해제될때 동작할 메서드
+		sessions.remove(session);
+		userSessionMap.remove(session.getId());
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		String msg = message.getPayload();
+		String[] strs = msg.split(",");
+		String userId = strs[0];
+		String text = strs[1];
+		String receiveId = strs[2];
+		
+		WebSocketSession receiveSession = userSessionMap.get(receiveId);
+		TextMessage textMessage = new TextMessage(userId+" : "+text);
+		receiveSession.sendMessage(textMessage);
+		
+		
+	}
+	
+	
+	
+	private String getUserId(WebSocketSession session) {
+		//session에서 로그인한 회원은 아이디를 리턴하고 로그인 안한 세션은 세션 아이디 반환
+		Map<String, Object> httpSession = session.getAttributes();
+		return httpSession.get("userInfo") == null ? session.getId() : ((Member)httpSession.get("userInfo")).getMemberId();
+	}
+}
--- menupam/src/main/webapp/resources/js/main.js
+++ menupam/src/main/webapp/resources/js/main.js
@@ -1,14 +1,2 @@
-/**
- * 
- */
 
-//우측 상단 알림벨 클릭 시 알림창 open/close 작업
 
-//1. 열렸는지 닫혔는지 확인할 변수
-let open = false;
-
-let notice = document.querySelector(".notice")
-
-notice.addEventListener("click",(e)=>{
-    console.log("test");
-})
