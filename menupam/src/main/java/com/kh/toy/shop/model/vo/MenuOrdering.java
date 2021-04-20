cababa1efd44f3fc7d0f6638b328a8e7f4dc2cd1
parkhyunwookk <pol1017@naver.com>
2021-04-21 05:08:38 +0900

    [박현욱] MenuOrdering 추가

---
+++ menupam/src/main/java/com/kh/toy/shop/model/vo/MenuOrdering.java
@@ -0,0 +1,58 @@
+package com.kh.toy.shop.model.vo;
+
+public class MenuOrdering {
+	
+	private String moIdx;
+	private String orderMenuCnt;
+	private String orderIdx;
+	private String menuIdx;
+	private String orderMenuName;
+	
+	public String getMoIdx() {
+		return moIdx;
+	}
+	public void setMoIdx(String moIdx) {
+		this.moIdx = moIdx;
+	}
+	
+	public String getOrderMenuCnt() {
+		return orderMenuCnt;
+	}
+	
+	public void setOrderMenuCnt(String orderMenuCnt) {
+		this.orderMenuCnt = orderMenuCnt;
+	}
+	
+	public String getOrderIdx() {
+		return orderIdx;
+	}
+	
+	public void setOrderIdx(String orderIdx) {
+		this.orderIdx = orderIdx;
+	}
+	
+	public String getMenuIdx() {
+		return menuIdx;
+	}
+	
+	public void setMenuIdx(String menuIdx) {
+		this.menuIdx = menuIdx;
+	}
+	
+	public String getOrderMenuName() {
+		return orderMenuName;
+	}
+	
+	public void setOrderMenuName(String orderMenuName) {
+		this.orderMenuName = orderMenuName;
+	}
+	
+	@Override
+	public String toString() {
+		return "MenuOrdering [moIdx=" + moIdx + ", orderMenuCnt=" + orderMenuCnt + ", orderIdx=" + orderIdx
+				+ ", menuIdx=" + menuIdx + ", orderMenuName=" + orderMenuName + "]";
+	}
+	
+	
+	
+}
